c8be2fca72d5270070fff5d2f8cd04692c02eb6e
xOtic | EVIL <58539553+likith18@users.noreply.github.com>
2024-06-22 14:31:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 document.addEventListener("DOMContentLoaded", function(){
-    // gsap.to(".sticky", {
-    //     scrollTrigger: {
-    //         trigger: ".sticky",
-    //         start: "top top",
-    //         end: () =>
-    //             "+=" +
-    //             (window.innerHeight + 
-    //              document.querySelector(".website-content").offsetHeight * 0.5),
-    //         srub:1,
-    //         pin:true,
-    //     },
-    //     y: 250,
-    //     scale: 0.75,
-    //     rotation: -15,
-    //     ease: "power3.out",
-    // });
+
+    gsap.to(".sticky", {
+        scrollTrigger: {
+            trigger: ".sticky",
+            start: "top top",
+            end: () =>
+                "+=" +
+                (window.innerHeight + 
+                 document.querySelector(".website-content").offsetHeight * 0.5),
+            scrub: 1, // Fixed typo here
+            pin:true,
+        },
+        y: 250,
+        scale: 0.75,
+        rotation: -15,
+        ease: "power3.out",
+    });
 
     gsap.fromTo(
         ".website-content", 
@@ -37,6 +38,7 @@ document.addEventListener("DOMContentLoaded", function(){
         }   
     );
 });
+
 
 // Function to check if an element is in viewport
 function isInViewport(element) {
